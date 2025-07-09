faddbf63f0e2e3f5e40b4d98338a3e10a833a61f
haroonkamaal <haroonkamaal26001@gmail.com>
2025-07-09 11:37:16 +0530

    Add: config env

--- src/factory/WeatherFactory.ts
+++ src/factory/WeatherFactory.ts
@@ -1,9 +1,11 @@
 import { HandleResponse } from '../services/HandleResponse';
 import { FetchResponseDto } from '../services/Type';
+import Config from 'react-native-config';
 
 class WeatherFactory {
   getCityWeather = async (city: string): Promise<FetchResponseDto> => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fd2510bac3bc8b72ae646bb593c047d7&units=metric`;
+    const key = Config.OPEN_WEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
     const result = await fetch(url);
     const data: FetchResponseDto = await HandleResponse(result);
     return data;
